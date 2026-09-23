14dca396aaddea2fffdc2c14402a47df11f47bdd
PintoJR <guimoraespinto@gmail.com>
2026-09-23 03:22:03 -0300

    Solution11

--- app/main.py
+++ app/main.py
@@ -3,20 +3,15 @@ from .cafe import Cafe
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
-
     masks_to_buy = 0
-
     try:
         for friend in friends:
             cafe.visit_cafe(friend)
-
     except VaccineError:
         return "All friends should be vaccinated"
-
-    except NotWearingMaskError:
+    else:
         masks_to_buy = sum(1 for f in friends
                            if not f.get("wearing_a_mask", False))
-
-        return f"Friends should buy {masks_to_buy} masks"
-
+        if masks_to_buy > 0:
+            return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
