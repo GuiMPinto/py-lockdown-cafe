e515b81daa055c05c7223b4af3adf0b608448d63
PintoJR <guimoraespinto@gmail.com>
2026-09-23 02:38:03 -0300

    Solution07

--- app/main.py
+++ app/main.py
@@ -14,9 +14,8 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         return "All friends should be vaccinated"
 
     except NotWearingMaskError:
-        masks_to_buy = sum(1 for f in friends 
+        masks_to_buy = sum(1 for f in friends
                            if not f.get("wearing_a_mask", False))
-
 
         return f"Friends should buy {masks_to_buy} masks"
 
